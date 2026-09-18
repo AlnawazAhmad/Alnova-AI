ad54e4b2ea353f5bdc6905d89ba17c041d8cc408
Syed Alnawaz Ahmad <syedalnawazahmad@gmail.com>
2026-09-18 23:27:08 +0530

    Update API_URL to production server

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // ALNOVA AI - MAIN JAVASCRIPT
 // ==========================================
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://alnova-ai.onrender.com";
 
 
 // ==========================================
